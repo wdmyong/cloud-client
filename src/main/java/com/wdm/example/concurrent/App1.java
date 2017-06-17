8c52a19abec1399b55eeb7be66533d96871c0c4b
yongduan <yongduan@sohu-inc.com>
2017-06-17 12:06:50 +0800

    add concurrent

---
+++ src/main/java/com/wdm/example/concurrent/App1.java
@@ -0,0 +1,30 @@
+package com.wdm.example.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App1 extends Thread {
+
+    private int index;
+    public App1(int i){
+        this.index=i;
+    }
+    public void run(){
+        try {
+            System.out.println("["+this.index+"] start....");
+            Thread.sleep((int)(Math.random()*1000));
+            System.out.println("["+this.index+"] end.");
+        } catch (Exception e)  {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String args[]){
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        for(int i=0;i<10;i++){
+            service.execute(new App1(i));
+            //service.submit(new MyExecutor(i));
+        }
+        System.out.println("submit finish");
+        service.shutdown();
+    }
+}
